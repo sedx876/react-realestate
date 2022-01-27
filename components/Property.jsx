61a93b0e9dc3a5ea186373d3ed1a1a27651558e0
sedx876 <swatkins876@gmail.com>
2022-01-27 09:50:55 -0500

    update

--- components/Property.jsx
+++ components/Property.jsx
@@ -1,13 +1,13 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import { Box, Flex, Text } from '@chakra-ui/layout';
-import { Avatar } from '@chakra-ui/avatar';
-import { FaBed, FaBath } from 'react-icons/fa';
-import { BsGridFill } from 'react-icons/bs';
-import { GoVerified } from 'react-icons/go';
-import millify from 'millify';
+import Link from 'next/link'
+import Image from 'next/image'
+import { Box, Flex, Text } from '@chakra-ui/layout'
+import { Avatar } from '@chakra-ui/avatar'
+import { FaBed, FaBath } from 'react-icons/fa'
+import { BsGridFill } from 'react-icons/bs'
+import { GoVerified } from 'react-icons/go'
+import millify from 'millify'
 
-import DefaultImage from '../assets/images/house.jpg';
+import DefaultImage from '../assets/images/house.jpeg'
 
 const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalID  } }) => (
   <Link href={`/property/${externalID}`} passHref>
@@ -37,4 +37,4 @@ const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, 
   </Link>
 );
 
-export default Property;
+export default Property
